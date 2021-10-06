60acd4b9219cf4ecf08ddc9ba2040b1dfdf683c4
Diego F. Ticona Ramos <dticona@agetic.gob.bo>
2021-10-06 16:24:55 -0400

    carrito completado

--- src/components/Carrito.vue
+++ src/components/Carrito.vue
@@ -5,68 +5,52 @@
   </div> -->
   <!-- <div class="container mx-auto mt-10">
   </div> -->
-  <div class="flex">
+  <div v-if="store.state.products.length > 0" class="flex">
     <div class="w-4/6 bg-white px-10 py-10">
       <div class="flex justify-between border-b pb-8">
         <h1 class="font-semibold text-2xl">Carrito de compra</h1>
-        <h2 class="font-semibold text-2xl">{{store.state.products.length}} Item{{store.state.products.length !== 1 ? 's' : ''}}</h2>
+        <!-- <h2 class="font-semibold text-2xl">{{store.state.products.length}} Item{{store.state.products.length !== 1 ? 's' : ''}}</h2> -->
       </div>
       <div class="flex mt-10 mb-5">
-        <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Detalles</h3>
+        <h3 class="font-semibold text-gray-600 text-xs uppercase w-2/5">Descripción</h3>
         <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Cantidad</h3>
         <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Precio</h3>
         <h3 class="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Total</h3>
       </div>
 
-      <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
+      <!-- inicio -->
+      <div v-for="(producto, index) in productos" :key="index" class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5 border-b">
         <div class="flex w-2/5">
           <div class="flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
-            </svg>
-            <span class="text-md font-medium w-auto">Inspección Técnica Vehicular</span>
-            <!-- <div class="flex flex-col ml-3 ">
-            </div> -->
+            </svg> -->
+            <span class="text-md font-medium w-auto">
+              {{ producto.descripcion }}
+            </span>
           </div>
         </div>
         <div class="flex justify-center w-1/5">
-          <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-          </svg>
-
-          <input class="mx-2 border text-center w-8" type="text" value="1">
-
-          <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-          </svg>
-        </div>
-        <span class="text-center w-1/5 font-semibold text-sm">Bs 400.00</span>
-        <span class="text-center w-1/5 font-semibold text-sm">Bs 400.00</span>
-      </div>
-
-      <div class="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-        <div class="flex w-2/5">
-          <div class="inline-flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+          <button @click="descontarCantidadProducto(index, producto.precioUnitario)" :disabled="producto.cantidad === 1" :class="{ 'text-gray-300': producto.cantidad === 1, 'text-gray-600': producto.cantidad > 1 }">
+            <svg class="fill-current w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
             </svg>
-            <span class="text-md font-medium">Folio Real Actualizado</span>
-            <!-- <div class="flex flex-col ml-3 ">
-            </div> -->
-          </div>
-        </div>
-        <div class="flex justify-center w-1/5">
-          <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-          </svg>
+          </button>
 
-          <input class="mx-2 border text-center w-8" type="text" value="1">
+          <!-- <input class="mx-2 border text-center w-8" type="text" :value="producto.cantidad"> -->
+          <span class="mx-2 border text-center w-8">{{ producto.cantidad }}</span>
 
-          <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-            <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
-          </svg>
+          <button @click="aumentarCantidadProducto(index, producto.precioUnitario)">
+            <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
+              <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+            </svg>
+          </button>
         </div>
-        <span class="text-center w-1/5 font-semibold text-sm">Bs 200.00</span>
-        <span class="text-center w-1/5 font-semibold text-sm">Bs 200.00</span>
+        <span class="text-center w-1/5 font-semibold text-sm">Bs {{ producto.precioUnitario }}</span>
+        <span class="text-center w-1/5 font-semibold text-sm">Bs {{ producto.subTotal }}</span>
+        <!-- <span class="text-center w-1/5 font-semibold text-sm"></span> -->
       </div>
+      <!-- fin -->
+
       <!-- <a href="#" class="flex font-semibold text-indigo-600 text-sm mt-10">
 
         <svg class="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z"/></svg>
@@ -78,7 +62,7 @@
       <h1 class="font-semibold text-2xl border-b pb-8">Detalle de compra</h1>
       <div class="flex justify-between mt-5 mb-5 text-gray-500">
         <span class="font-medium text-sm">Subtotal</span>
-        <span class="font-medium text-sm">Bs. 600</span>
+        <span class="font-medium text-sm">Bs. {{ costoTotal }}</span>
       </div>
       <!-- <div>
         <label class="font-medium inline-block mb-3 text-sm uppercase">Shipping</label>
@@ -89,26 +73,100 @@
       <div class="border-t mt-5">
         <div class="flex font-semibold justify-between py-6 text-sm">
           <span>Costo total</span>
-          <span>Bs. 600</span>
+          <span>Bs. {{ costoTotal }}</span>
         </div>
-        <button class="bg-primary font-semibold hover:bg-opacity-70 py-3 text-sm text-white uppercase w-full rounded-md">PAGAR</button>
+        <button class="bg-primary font-semibold hover:bg-opacity-70 py-3 text-sm text-white uppercase w-full border-0 rounded-md focus:outline-none focus:ring-4 focus:ring-primary">
+          PAGAR
+        </button>
       </div>
     </div>
+  </div>
+  <div v-else class="text-center pt-28">
+    <h1 class="text-5xl text-gray-700">El carrito de compras esta vacío</h1>
   </div>
 
 </template>
 
 <script>
 import imgTramite from '/src/assets/tramite.svg'
-import { inject } from 'vue'
+import { inject, onMounted, ref, watchEffect } from 'vue'
 
 export default {
   setup() {
     const store = inject('store')
+    const productos = ref([])
+    const costoTotal = ref(0)
+
+    onMounted(async () => {
+      console.log('carrito mounted!')
+      procesarCarrito()
+    })
+
+    // watchEffect(() => {})
+
+    const procesarCarrito = () => {
+      console.log('store.state.products :: ', store.state.products);
+      const tmpProducts = [ ...store.state.products ];
+      let contadorProductos = [];
+      // tmpProducts.findIndex()
+      console.log('tmpProducts :: ', tmpProducts);
+      let _costoTotal = 0;
+      tmpProducts.forEach((producto) => {
+        _costoTotal += producto.precioUnitario;
+        const idxProducto = contadorProductos.findIndex((el) => el.codigo === producto.codigo);
+        console.log('existe producto ? ', idxProducto !== -1);
+        if (idxProducto !== -1) {
+          // el producto ya esta en el carrito
+          const productTmp = {...contadorProductos[idxProducto]}
+          productTmp.cantidad += 1;
+          productTmp.subTotal += producto.precioUnitario;
+          contadorProductos[idxProducto] = {...productTmp}
+        } else {
+          // insertamos la primera vez el producto en el carrito
+          contadorProductos.push({
+            ...producto,
+            cantidad: 1,
+            subTotal: producto.precioUnitario
+          })
+        }
+      })
+
+      console.log('CARRITO PROCESADO > ', contadorProductos);
+      productos.value = contadorProductos;
+      costoTotal.value = _costoTotal;
+    }
+
+    const aumentarCantidadProducto = (idxProducto, precioUnitario) => {
+      console.log('Incrementando cantidad de producto....');
+      const tmpProductos = [...productos.value];
+      // const tmpProducto = {...tmpProductos[idxProducto]};
+      tmpProductos[idxProducto].cantidad += 1;
+      tmpProductos[idxProducto].subTotal += precioUnitario;
+      productos.value = tmpProductos;
+      costoTotal.value += precioUnitario;
+    }
+
+    const descontarCantidadProducto = (idxProducto, precioUnitario) => {
+      console.log('Incrementando cantidad de producto....');
+      const tmpProductos = [...productos.value];
+      // const tmpProducto = {...tmpProductos[idxProducto]};
+      // if (tmpProductos[idxProducto].cantidad > 1) {
+      //   }
+      tmpProductos[idxProducto].cantidad -= 1;
+      tmpProductos[idxProducto].subTotal -= precioUnitario;
+      productos.value = tmpProductos;
+      costoTotal.value -= precioUnitario;
+    }
 
     return {
+      // params
       imgTramite,
-      store
+      store,
+      productos,
+      // Functions
+      aumentarCantidadProducto,
+      descontarCantidadProducto,
+      costoTotal
     }
   },
 }
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,9 +8,9 @@
     <div class="flex flex-col space-y-3 p-4">
       <img class="w-16" :src=imgTramite alt="img-tramite">
       <h1 class="text-base">Inspección Técnica Vehicular</h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 360</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 1, tramite: 'Inspección Técnica Vehicular'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 360</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 1, codigo: '1AB22', codigoSin: '102021', unidadMedida: 57, precioUnitario: 360, descripcion: 'Inspección Técnica Vehicular'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
@@ -22,9 +22,9 @@
       <h1 class="text-base">
         Renovación de Licencia para Conducir – Categoría M, P, T, A, B y C
       </h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 120</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 2, tramite: 'Renovación de Licencia para Conducir – Categoría M, P, T, A, B y C'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 122.50</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 2, codigo: '2AB22', codigoSin: '202021', unidadMedida: 57, precioUnitario: 122.50, descripcion: 'Renovación de Licencia para Conducir – Categoría M, P, T, A, B y C'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
@@ -36,9 +36,9 @@
       <h1 class="text-base">
         Certificado de Antecedentes Policiales
       </h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 80</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 3, tramite: 'Certificado de Antecedentes Policiales'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 80</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 3, codigo: '3AB22', codigoSin: '302021', unidadMedida: 57, precioUnitario: 88, descripcion: 'Certificado de Antecedentes Policiales'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
@@ -50,9 +50,9 @@
       <h1 class="text-base">
         Folio Real Actualizado
       </h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 55.40</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 4, tramite: 'Folio Real Actualizado'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 55.40</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 4, codigo: '4AB22', codigoSin: '402021', unidadMedida: 57, precioUnitario: 55.40, descripcion: 'Folio Real Actualizado'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
@@ -64,9 +64,9 @@
       <h1 class="text-base">
         Legalización del certificado de egreso de auxiliares y técnicos medios en salud
       </h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 88</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 5, tramite: 'Legalización del certificado de auxiliares y técnicos medios en salud'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 587.70</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 5, codigo: '5AB22', codigoSin: '502021', unidadMedida: 57, precioUnitario: 587.70, descripcion: 'Legalización del certificado de auxiliares y técnicos medios en salud'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
@@ -78,9 +78,9 @@
       <h1 class="text-base">
         Registro plurinacional de artistas
       </h1>
-      <div class="flex space-x-3 items-center text-secondary">
-        <span class="text-lg font-semibold">Bs. 150</span>
-        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 6, tramite: 'Registro plurinacional de artistas'})">
+      <div class="flex space-x-3 items-center text-secondary bg-gray-100 bg-opacity-70 rounded-lg p-1">
+        <span class="text-lg font-semibold flex-1">Bs. 150</span>
+        <button class="text-secondary hover:text-opacity-70" @click="store.methods.addProduct({id: 6, codigo: '6AB22', codigoSin: '602021', unidadMedida: 57, precioUnitario: 150, descripcion: 'Registro plurinacional de artistas'})">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-11a1 1 0 10-2 0v2H7a1 1 0 100 2h2v2a1 1 0 102 0v-2h2a1 1 0 100-2h-2V7z" clip-rule="evenodd" />
           </svg>
